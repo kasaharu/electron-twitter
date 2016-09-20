dd72c42210801c5514593a96ee887cd15ed5ae94
Wataru.Kasahara <kasahara1019@gmail.com>
2016-09-20 21:41:17 +0900

    Open the tweet window via tray

--- src/app/javascripts/main/main.js
+++ src/app/javascripts/main/main.js
@@ -2,12 +2,14 @@ const { app } = require('electron');
 const TimelineWindow = require('./timeline-window');
 const FormWindow = require('./form-window');
 const AppMenu = require('./app-menu');
+const AppTray = require('./app-tray');
 
 class Main {
   constructor() {
     this.timelineWindow = new TimelineWindow();
     this.formWindow = new FormWindow();
     this.start();
+    this.appTray = new AppTray();
   }
 
   start() {
